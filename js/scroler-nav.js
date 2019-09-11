e9a9901db9b6399b07e98a73a09b123c0d970610
Muhammad Qamar Aftab <muhammad_qamar_aftab@snl.int>
2019-09-11 20:27:32 +0500

    initial

---
+++ js/scroler-nav.js
@@ -0,0 +1,444 @@
+	//overview
+	document.getElementById('overview-preface').addEventListener('mouseenter',function(){
+ $('#overview-account-structure1').parent().children().removeClass('overview-txt-clicked')
+       $('#overview-preface1').addClass('overview-txt-clicked');
+})
+document.getElementById('overview-preface').addEventListener('mouseleave',function(){
+ $('#overview-preface1').addClass('overview-txt-clicked');
+     
+
+})
+
+document.getElementById('overview-product-hierarchy').addEventListener('mouseenter',function(){
+ $('#overview-account-structure1').parent().children().removeClass('overview-txt-clicked')
+       $('#overview-product-hierarchy1').addClass('overview-txt-clicked');
+})
+document.getElementById('overview-product-hierarchy').addEventListener('mouseleave',function(){
+
+ $('#overview-preface1').addClass('overview-txt-clicked');
+       $('#overview-product-hierarchy1').removeClass('overview-txt-clicked');
+
+})
+
+document.getElementById('overview-account-structure').addEventListener('mouseenter',function(){
+ $('#overview-account-structure1').parent().children().removeClass('overview-txt-clicked')
+       $('#overview-account-structure1').addClass('overview-txt-clicked');
+})
+document.getElementById('overview-account-structure').addEventListener('mouseleave',function(){
+ $('#overview-preface1').addClass('overview-txt-clicked');
+       $('#overview-account-structure1').removeClass('overview-txt-clicked');
+
+})
+
+
+	//started
+	document.getElementById('getting-signing-up').addEventListener('mouseenter',function(){
+ $('#getting-signing-up1').parent().children().removeClass('overview-txt-clicked')
+       $('#getting-signing-up1').addClass('overview-txt-clicked');
+})
+document.getElementById('getting-signing-up').addEventListener('mouseleave',function(){
+ $('#getting-signing-up').addClass('overview-txt-clicked');
+     
+
+})
+
+document.getElementById('getting-program-api').addEventListener('mouseenter',function(){
+ $('#getting-signing-up1').parent().children().removeClass('overview-txt-clicked')
+       $('#getting-program-api1').addClass('overview-txt-clicked');
+})
+document.getElementById('getting-program-api').addEventListener('mouseleave',function(){
+
+ $('#getting-signing-up1').addClass('overview-txt-clicked');
+       $('#getting-program-api1').removeClass('overview-txt-clicked');
+
+})
+
+document.getElementById('getting-api-call-response').addEventListener('mouseenter',function(){
+ $('#getting-signing-up1').parent().children().removeClass('overview-txt-clicked')
+       $('#getting-api-call-response1').addClass('overview-txt-clicked');
+})
+document.getElementById('getting-api-call-response').addEventListener('mouseleave',function(){
+ $('#getting-signing-up1').addClass('overview-txt-clicked');
+       $('#getting-api-call-response1').removeClass('overview-txt-clicked');
+
+})
+
+	//sandbox
+	document.getElementById('sandbox-signing-up').addEventListener('mouseenter',function(){
+ $('#sandbox-signing-up1').parent().children().removeClass('overview-txt-clicked')
+       $('#sandbox-signing-up1').addClass('overview-txt-clicked');
+})
+document.getElementById('sandbox-signing-up').addEventListener('mouseleave',function(){
+ $('#getting-signing-up1').addClass('overview-txt-clicked');
+     
+
+})
+
+document.getElementById('sandbox-using-api').addEventListener('mouseenter',function(){
+ $('#sandbox-signing-up1').parent().children().removeClass('overview-txt-clicked')
+       $('#sandbox-using-api1').addClass('overview-txt-clicked');
+})
+document.getElementById('sandbox-using-api').addEventListener('mouseleave',function(){
+
+ $('#sandbox-signing-up1').addClass('overview-txt-clicked');
+       $('#sandbox-using-api1').removeClass('overview-txt-clicked');
+
+})
+
+	//account
+	document.getElementById('account-tab-preface').addEventListener('mouseenter',function(){
+ $('#account-tab-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#account-tab-preface1').addClass('overview-txt-clicked');
+})
+document.getElementById('getting-signing-up').addEventListener('mouseleave',function(){
+ $('#account-tab-preface1').addClass('overview-txt-clicked');
+     
+
+})
+
+document.getElementById('account-api-link').addEventListener('mouseenter',function(){
+ $('#account-tab-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#account-api-link1').addClass('overview-txt-clicked');
+})
+document.getElementById('account-api-link').addEventListener('mouseleave',function(){
+
+ $('#account-tab-preface1').addClass('overview-txt-clicked');
+       $('#account-api-link1').removeClass('overview-txt-clicked');
+
+})
+
+document.getElementById('ach-account').addEventListener('mouseenter',function(){
+ $('#account-tab-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#ach-account1').addClass('overview-txt-clicked');
+})
+document.getElementById('ach-account').addEventListener('mouseleave',function(){
+ $('#account-tab-preface1').addClass('overview-txt-clicked');
+       $('#ach-account1').removeClass('overview-txt-clicked');
+
+})
+
+//cards
+
+	document.getElementById('card-preface').addEventListener('mouseenter',function(){
+ $('#card-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#card-preface1').addClass('overview-txt-clicked');
+})
+document.getElementById('card-preface').addEventListener('mouseleave',function(){
+ $('#card-preface1').addClass('overview-txt-clicked');
+     
+
+})
+
+document.getElementById('card-apis').addEventListener('mouseenter',function(){
+ $('#card-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#card-apis1').addClass('overview-txt-clicked');
+})
+document.getElementById('card-apis').addEventListener('mouseleave',function(){
+
+ $('#card-preface1').addClass('overview-txt-clicked');
+       $('#card-apis1').removeClass('overview-txt-clicked');
+
+})
+
+document.getElementById('card-virtual').addEventListener('mouseenter',function(){
+ $('#card-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#card-virtual1').addClass('overview-txt-clicked');
+})
+document.getElementById('card-virtual').addEventListener('mouseleave',function(){
+ $('#card-preface1').addClass('overview-txt-clicked');
+       $('#card-virtual1').removeClass('overview-txt-clicked');
+
+})
+
+document.getElementById('card-inventory').addEventListener('mouseenter',function(){
+ $('#card-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#card-inventory1').addClass('overview-txt-clicked');
+})
+document.getElementById('card-inventory').addEventListener('mouseleave',function(){
+ $('#card-preface1').addClass('overview-txt-clicked');
+       $('#card-inventory1').removeClass('overview-txt-clicked');
+
+})
+
+//transaction
+
+
+//1
+
+document.getElementById('transaction-preface').addEventListener('mouseenter',function(){
+ $('#transaction-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#transaction-preface1').addClass('overview-txt-clicked');
+})
+document.getElementById('transaction-preface').addEventListener('mouseleave',function(){
+
+ $('#transaction-preface1').addClass('overview-txt-clicked');
+  
+
+})
+//2
+document.getElementById('transaction-apis').addEventListener('mouseenter',function(){
+ $('#transaction-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#transaction-apis1').addClass('overview-txt-clicked');
+})
+document.getElementById('transaction-apis').addEventListener('mouseleave',function(){
+ $('#transaction-preface1').addClass('overview-txt-clicked');
+       $('#transaction-apis1').removeClass('overview-txt-clicked');
+
+})
+//3
+document.getElementById('transaction-adjustment').addEventListener('mouseenter',function(){
+ $('#transaction-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#transaction-adjustment1').addClass('overview-txt-clicked');
+})
+document.getElementById('transaction-adjustment').addEventListener('mouseleave',function(){
+ $('#transaction-preface1').addClass('overview-txt-clicked');
+       $('#transaction-adjustment1').removeClass('overview-txt-clicked');
+
+})
+//4
+document.getElementById('transaction-payment').addEventListener('mouseenter',function(){
+ $('#transaction-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#transaction-payment1').addClass('overview-txt-clicked');
+})
+document.getElementById('transaction-payment').addEventListener('mouseleave',function(){
+ $('#transaction-preface1').addClass('overview-txt-clicked');
+       $('#transaction-payment1').removeClass('overview-txt-clicked');
+
+})
+//5
+document.getElementById('transaction-hold').addEventListener('mouseenter',function(){
+ $('#transaction-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#transaction-hold1').addClass('overview-txt-clicked');
+})
+document.getElementById('transaction-hold').addEventListener('mouseleave',function(){
+ $('#transaction-preface1').addClass('overview-txt-clicked');
+       $('#transaction-hold1').removeClass('overview-txt-clicked');
+
+})
+//6
+document.getElementById('transaction-merchant-credits').addEventListener('mouseenter',function(){
+ $('#transaction-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#transaction-merchant-credits1').addClass('overview-txt-clicked');
+})
+document.getElementById('transaction-merchant-credits').addEventListener('mouseleave',function(){
+ $('#transaction-preface1').addClass('overview-txt-clicked');
+       $('#transaction-merchant-credits1').removeClass('overview-txt-clicked');
+
+})
+
+//location
+//transaction
+
+
+//1
+
+document.getElementById('facebook_ads_example').addEventListener('mouseenter',function(){
+ $('#facebook_ads_example1').parent().children().removeClass('overview-txt-clicked')
+       $('#facebook_ads_example1').addClass('overview-txt-clicked');
+})
+document.getElementById('facebook_ads_example').addEventListener('mouseleave',function(){
+
+ $('#facebook_ads_example1').addClass('overview-txt-clicked');
+     
+
+})
+//2
+document.getElementById('facebook_adss_example').addEventListener('mouseenter',function(){
+ $('#facebook_ads_example1').parent().children().removeClass('overview-txt-clicked')
+       $('#facebook_adss_example1').addClass('overview-txt-clicked');
+})
+document.getElementById('facebook_adss_example').addEventListener('mouseleave',function(){
+
+ $('#facebook_ads_example1').addClass('overview-txt-clicked');
+       $('#facebook_adss_example1').removeClass('overview-txt-clicked');
+
+})
+
+//ivr
+
+
+//1
+
+document.getElementById('ivr-overview').addEventListener('mouseenter',function(){
+ $('#ivr-overview1').parent().children().removeClass('overview-txt-clicked')
+       $('#ivr-overview1').addClass('overview-txt-clicked');
+})
+document.getElementById('ivr-overview').addEventListener('mouseleave',function(){
+
+ $('#ivr-overview1').addClass('overview-txt-clicked');
+     
+
+})
+//2
+document.getElementById('ivr-apis').addEventListener('mouseenter',function(){
+ $('#ivr-overview1').parent().children().removeClass('overview-txt-clicked')
+       $('#ivr-apis1').addClass('overview-txt-clicked');
+})
+document.getElementById('ivr-apis').addEventListener('mouseleave',function(){
+
+ $('#ivr-overview1').addClass('overview-txt-clicked');
+       $('#ivr-apis').removeClass('overview-txt-clicked');
+
+})
+////////////////////////////
+
+//bill payment
+
+
+//1
+
+document.getElementById('bill-payment-preface').addEventListener('mouseenter',function(){
+ $('#bill-payment-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#bill-payment-preface1').addClass('overview-txt-clicked');
+})
+document.getElementById('transaction-preface').addEventListener('mouseleave',function(){
+
+ $('#bill-payment-preface1').addClass('overview-txt-clicked');
+      
+
+})
+//2
+document.getElementById('bill-payment-paper').addEventListener('mouseenter',function(){
+$('#bill-payment-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#bill-payment-paper1').addClass('overview-txt-clicked');
+})
+document.getElementById('bill-payment-paper').addEventListener('mouseleave',function(){
+ $('#bill-payment-preface1').addClass('overview-txt-clicked');
+       $('#bill-payment-paper1').removeClass('overview-txt-clicked');
+
+})
+//3
+document.getElementById('bill-payment-paper').addEventListener('mouseenter',function(){
+$('#bill-payment-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#bill-payment-paper1').addClass('overview-txt-clicked');
+})
+document.getElementById('bill-payment-paper').addEventListener('mouseleave',function(){
+ $('#bill-payment-preface1').addClass('overview-txt-clicked');
+       $('#bill-payment-paper1').removeClass('overview-txt-clicked');
+
+})
+//4
+document.getElementById('bill-payment-rpps').addEventListener('mouseenter',function(){
+$('#bill-payment-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#bill-payment-rpps1').addClass('overview-txt-clicked');
+})
+document.getElementById('bill-payment-rpps').addEventListener('mouseleave',function(){
+ $('#bill-payment-preface1').addClass('overview-txt-clicked');
+       $('#bill-payment-rpps1').removeClass('overview-txt-clicked');
+
+})
+//5
+document.getElementById('bill-payment-adding-biller').addEventListener('mouseenter',function(){
+$('#bill-payment-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#bill-payment-adding-biller1').addClass('overview-txt-clicked');
+})
+document.getElementById('bill-payment-adding-biller').addEventListener('mouseleave',function(){
+ $('#bill-payment-preface1').addClass('overview-txt-clicked');
+       $('#bill-payment-adding-biller1').removeClass('overview-txt-clicked');
+
+})
+
+////////////////////////////
+
+//program and product managment
+
+
+//1
+
+document.getElementById('ppm-preface').addEventListener('mouseenter',function(){
+ $('#ppm-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#ppm-preface1').addClass('overview-txt-clicked');
+})
+document.getElementById('ppm-preface').addEventListener('mouseleave',function(){
+
+ $('#ppm-preface1').addClass('overview-txt-clicked');
+      
+
+})
+//2
+document.getElementById('ppm-apis').addEventListener('mouseenter',function(){
+$('#ppm-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#ppm-apis1').addClass('overview-txt-clicked');
+})
+document.getElementById('ppm-apis').addEventListener('mouseleave',function(){
+ $('#ppm-preface1').addClass('overview-txt-clicked');
+       $('#ppm-apis1').removeClass('overview-txt-clicked');
+
+})
+
+
+
+////////////////////////////////////////
+//purchase accont managment
+
+
+//1
+
+document.getElementById('pam-preface').addEventListener('mouseenter',function(){
+ $('#pam-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#pam-preface1').addClass('overview-txt-clicked');
+})
+document.getElementById('pam-preface').addEventListener('mouseleave',function(){
+
+ $('#pam-preface1').addClass('overview-txt-clicked');
+      
+
+})
+//2
+document.getElementById('pam-apis').addEventListener('mouseenter',function(){
+ $('#pam-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#pam-apis1').addClass('overview-txt-clicked');
+})
+document.getElementById('pam-apis').addEventListener('mouseleave',function(){
+ $('#pam-preface1').addClass('overview-txt-clicked');
+       $('#pam-apis1').removeClass('overview-txt-clicked');
+
+})
+//3
+document.getElementById('pam-sweepoff').addEventListener('mouseenter',function(){
+ $('#pam-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#pam-sweepoff1').addClass('overview-txt-clicked');
+})
+document.getElementById('pam-sweepoff').addEventListener('mouseleave',function(){
+ $('#pam-preface1').addClass('overview-txt-clicked');
+       $('#pam-sweepoff1').removeClass('overview-txt-clicked');
+
+})
+//4
+document.getElementById('pam-topup').addEventListener('mouseenter',function(){
+ $('#pam-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#pam-topup1').addClass('overview-txt-clicked');
+})
+document.getElementById('pam-topup').addEventListener('mouseleave',function(){
+ $('#pam-preface1').addClass('overview-txt-clicked');
+       $('#pam-topup1').removeClass('overview-txt-clicked');
+
+})
+
+////////////////////////////////////
+
+//federal benifit
+
+
+//1
+
+document.getElementById('fbe-preface').addEventListener('mouseenter',function(){
+ $('#fbe-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#fbe-preface1').addClass('overview-txt-clicked');
+})
+document.getElementById('fbe-preface').addEventListener('mouseleave',function(){
+
+ $('#fbe-preface1').addClass('overview-txt-clicked');
+      
+
+})
+//2
+document.getElementById('fbe-apis').addEventListener('mouseenter',function(){
+ $('#fbe-preface1').parent().children().removeClass('overview-txt-clicked')
+       $('#fbe-apis1').addClass('overview-txt-clicked');
+})
+document.getElementById('fbe-apis').addEventListener('mouseleave',function(){
+ $('#fbe-preface1').addClass('overview-txt-clicked');
+       $('#fbe-apis1').removeClass('overview-txt-clicked');
+
+})
