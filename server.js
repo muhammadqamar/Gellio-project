77d03759f60eb4e79e57624722a2bfb818722667
Muhammad Qamar Aftab <muhammad_qamar_aftab@snl.int>
2019-09-12 06:22:59 +0500

    changes

---
+++ server.js
@@ -0,0 +1,9 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
